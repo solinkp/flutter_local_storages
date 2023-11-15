979898daa3374edea406f3fac89f361cdc00999f
Ricardo Sanchez <rsanchezsk@gmail.com>
2023-11-15 12:19:13 -0600

    reordered based on ranking

--- lib/core/constants/storage_items.dart
+++ lib/core/constants/storage_items.dart
@@ -2,15 +2,15 @@ import 'package:flutter_local_storages/domain/storage_item/storage_item.dart';
 
 const List<StorageItem> noSqlStorageItems = [
   StorageItem(name: 'Isar'),
-  StorageItem(name: 'Hive'),
   StorageItem(name: 'Objectbox'),
   StorageItem(name: 'Sembast'),
-  StorageItem(name: 'CBL'),
   StorageItem(name: 'Realm'),
+  StorageItem(name: 'Hive'),
+  StorageItem(name: 'CBL'),
 ];
 
 const List<StorageItem> sqlStorageItems = [
-  StorageItem(name: 'SQFLite'),
   StorageItem(name: 'Floor'),
+  StorageItem(name: 'SQFLite'),
   StorageItem(name: 'Drift'),
 ];
